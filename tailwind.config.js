6d8a3de26c3f5279041259c1d3fd609892a68e7e
ariba <aribaonsocial@gmail.com>
2022-01-30 21:02:28 +0530

    done github finder

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+};
